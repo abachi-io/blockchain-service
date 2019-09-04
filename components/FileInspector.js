4cf5f0036be107eb6a8dcbd2f2bf04a386dfffe6
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-09-05 08:32:16 +1000

    if no .env make one

--- components/FileInspector.js
+++ components/FileInspector.js
@@ -12,7 +12,7 @@ class FileInspector {
         return;
       } else {
         console.log(chalk.yellow(`[+] Missing .env file, attempting to create`))
-          fs.writeFile('.env', `NETWORK=mainnet\nSERVER_PORT=9899`, (error) => {
+          fs.writeFile('.env', `WEB3_HTML=http://testnet.ledgerium.net:8545\nSERVER_PORT=9899`, (error) => {
           if (error) return reject({});
           console.log(chalk.green(`[+] Successfully created an .env file`))
         });
