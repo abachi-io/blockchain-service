541e4ec0527157fdd2f1faadf6d184e24e76ba96
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-09-15 23:13:02 +1000

    refactor api, add proof contract call api endpoints

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,14 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const Web3 = require('../components/Web3')
 const web3 = new Web3()
 const Invoice = require('../components/Invoice')
 const invoice = new Invoice(web3)
+const Contract = require('../components/Contract')
+const contract = new Contract()
+const Proof = require('../components/Proof')
+const proof = new Proof(web3)
 const InputDataDecoder = require('ethereum-input-data-decoder');
 
 const successResponse = (response, message = null, data = null) => {
@@ -23,9 +28,73 @@ const errorResponse = (response, message, status = 403) => {
   })
 }
 
+
+// General \\
+
 router.get('/ping', (request, response) => {
   return successResponse(response, 'pong')
 })
+
+// Proof Contract \\
+
+router.get('/proof/data/:key', (request, response) => {
+    const { key } = request.params
+    proof.get(key)
+      .then(payload => {
+        return successResponse(response, `CMD: get(${key})`, payload);
+      })
+      .catch(error => {
+        return errorResponse(response, error);
+      })
+})
+
+router.post('/proof/', (request, response) => {
+  const { key, store } = request.body;
+  proof.set(key, store)
+    .then(payload => {
+      return successResponse(response, `CMD: set(${key}, ${store})`, payload);
+    })
+    .catch(error => {
+      return errorResponse(response, error);
+    })
+
+})
+
+router.delete('/proof/', (request, response) => {
+  const { key } = request.body;
+  proof.remove(key, store)
+    .then(payload => {
+      return successResponse(response, `CMD: remove(${key})`, payload);
+    })
+    .catch(error => {
+      return errorResponse(response, error);
+    })
+})
+
+router.get('/proof/exists/:key', (request, response) => {
+    const { key } = request.params
+    proof.exists(key)
+      .then(payload => {
+        return successResponse(response, `CMD: exists(${key})`, payload);
+      })
+      .catch(error => {
+        return errorResponse(response, error);
+      })
+})
+
+router.get('/proof/timestamp/:key', (request, response) => {
+    const { key } = request.params
+    proof.timestamp(key)
+      .then(payload => {
+        return successResponse(response, `CMD: timestamp(${key})`, payload);
+      })
+      .catch(error => {
+        return errorResponse(response, error);
+      })
+})
+
+
+// Web3 Wrapper \\
 
 router.get('/transaction/:hash', (request, response) => {
   const { hash } = request.params
@@ -61,6 +130,7 @@ router.get('/pendingTransactions', (request, response) => {
     console.log(error)
     return errorResponse(response, error)
   })})
+
 
 router.get('/block/:blockNumber', (request, response) => {
   const { blockNumber } = request.params
@@ -110,7 +180,7 @@ router.get('/hash/:hash', (request, response) => {
 })
 
 router.get('/isHash/:hash', (request, response) => {
-  const {hash} = request.params
+  const { hash } = request.params
   invoice.existsOnBlockchain(hash)
     .then(payload => {
       return successResponse(response, `Check if ${hash} exists`, payload);
@@ -121,7 +191,7 @@ router.get('/isHash/:hash', (request, response) => {
 })
 
 router.post('/hash', (request, response) => {
-  const {invoiceId, hash} = request.body
+  const { invoiceId, hash } = request.body;
   invoice.storeOnBlockchain(invoiceId, hash)
     .then(payload => {
       return successResponse(response, `Stored Invoice ID: '${invoiceId}' with hash '${hash}'`, payload);
@@ -129,6 +199,36 @@ router.post('/hash', (request, response) => {
     .catch(error => {
       return errorResponse(response, error);
     })
+})
+
+
+router.get('/deploy/contract/:file', (request, response) => {
+  const { file } = request.params
+  contract.compile(file)
+    .then(data => {
+      console.log(data)
+      let contract = new web3.web3Http.eth.Contract(data.abi, {from: process.env.PUBLIC_KEY, gas: 47000, data: `0x${data.bytecode}`});
+      return successResponse(response, '', contract)
+      contract.deploy({})
+        .send({}, function(error, transactionHash){ console.log })
+        .on('error', function(error){ console.log })
+        .on('transactionHash', function(transactionHash){ console.log })
+        .on('receipt', function(receipt){
+           console.log(receipt.contractAddress) // contains the new contract address
+        })
+        .on('confirmation', function(confirmationNumber, receipt){ console.log })
+        .then(function(newContractInstance){
+            console.log(newContractInstance.options.address) // instance with the new contract address
+        })
+
+      successResponse(response, `Deployed contract`, data);
+
+    })
+    .catch(error => {
+      console.log(error)
+      return errorResponse(response, error);
+    })
+
 })
 
 module.exports = router;
