8dd71a7972510fa381754f6951fbeeb5c0dc19f4
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-09-19 22:06:26 +1000

    add custom web3 func

--- components/Web3.js
+++ components/Web3.js
@@ -5,6 +5,19 @@ class Web3 {
     constructor() {
       this.host = process.env.WEB3_HTTP || 'http://testnet.ledgerium.net:8545/'
       this.web3Http = new web3(new web3.providers.HttpProvider(this.host));
+      this.web3Http.eth.extend({
+        property: 'txpool',
+        methods: [{
+          name: 'content',
+          call: 'txpool_content'
+        },{
+          name: 'inspect',
+          call: 'txpool_inspect'
+        },{
+          name: 'status',
+          call: 'txpool_status'
+        }]
+      })
     }
 
 }
