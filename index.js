c72dd08e6da0ea54ff33f053d462ec8eb6ea02b1
Steve <admin@cryptostat.org>
2019-09-30 12:48:59 +1000

    default port is 9899

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cluster = require('cluster')
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.SERVER_PORT || 9999;
+const port = process.env.SERVER_PORT || 9899;
 
 const server = app.listen(port, () => {
   console.log(chalk.green(`[+] Listening on port: ${port}`))
