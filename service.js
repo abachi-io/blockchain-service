e7ecdf806260025e5a9147c188bca7d45efe06c7
Steve <admin@cryptostat.org>
2019-10-14 12:33:24 +1100

    replace console.logs with logger.info logger.debug and logger.error

--- service.js
+++ service.js
@@ -9,10 +9,8 @@ if (process.env.NODE_ENV === 'dev') {
     dotenv.config( { path: path.resolve(process.cwd(), `./environments/${process.env.NODE_ENV}/.env`) });
 }
 
-const chalk = require('chalk')
-
 const server = require('./lib/server.js')
 const port = process.env.SERVER_PORT || 9899;
 server.listen(port, () => {
-  console.log(chalk.green(`[+] Listening on port: ${port}`))
+  logger.info(`Listening on port ${port}`)
 })
